1abd88535089dc715e9eab6db5223eb5602398d3
Jardoe <jussi.g.pardoe@gmail.com>
2018-02-13 09:22:50 +0000

    changed time

--- homework.sql
+++ homework.sql
@@ -9,6 +9,7 @@ WHERE name = 'Kte Preston';
 SELECT name FROM people;
 
 SELECT name FROM people WHERE name = 'Jussi Pardoe';
+--SELECT name FROM people WHERE id = id of person
 
 DELETE FROM movies WHERE title = 'Batman Begins';
 
@@ -18,7 +19,7 @@ DELETE FROM people WHERE name = 'Jarrod Bennie';
 
 INSERT INTO movies(title, year, show_time) VALUES ('Guardians of the Galaxy 2', 2017, '00:00');
 
-UPDATE movies SET show_time = '18:55' WHERE title = 'Guardians of the Galaxy';
+UPDATE movies SET show_time = '21:30' WHERE title = 'Guardians of the Galaxy';
 
 DELETE FROM movies
 WHERE id IN (SELECT id FROM movies WHERE year < 2013);
